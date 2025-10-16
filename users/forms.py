7407a2755a2b198d29084889f78b9da2303e5121
Venci <vencislavpeev18.8@gmail.com>
2025-10-16 14:08:17 +0300

    fix imports for `users/forms.py`

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from users.models import User
+from users.models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ("username", "email")
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ("username", "password")
