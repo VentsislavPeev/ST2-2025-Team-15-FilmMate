e9be5ba733692040557ead7e7cb2efbad310533f
Venci <vencislavpeev18.8@gmail.com>
2025-10-21 18:54:54 +0300

    Implement friend request functionality with views, models, and templates

--- users/models.py
+++ users/models.py
@@ -15,3 +15,18 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class FriendRequest(models.Model):
+    """A simple friend request from one user to another."""
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_friend_requests', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_friend_requests', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['from_user', 'to_user'], name='unique_friend_request')
+        ]
+
+    def __str__(self):
+        return f"FriendRequest(from={self.from_user_id}, to={self.to_user_id})"
