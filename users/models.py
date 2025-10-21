48897015b39abc3ece8b673e5dce46b87130a89b
Venci <vencislavpeev18.8@gmail.com>
2025-10-21 18:29:05 +0300

    Add friends field to CustomUser model and create migration

--- users/models.py
+++ users/models.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
+    # Mutual friendships between users. blank=True allows no friends.
+    friends = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        related_name='friends_rel',
+        help_text='Users you are friends with'
+    )
+    
 
     def __str__(self):
         return self.username
